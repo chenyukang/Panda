2be9efe14f62e802c55359f65fc77a7ac2e98877
Yukang <moorekang@gmail.com>
2012-03-05 12:26:22 +0800

    improve build.sh

--- build.sh
+++ build.sh
@@ -10,42 +10,55 @@ KERNEL="./kernel"
 INCLUDE="./include"
 NASM="nasm -f elf"
 GCC="gcc -c -W -Wall -I./include/ -fno-stack-protector -fno-builtin"
-OBJS="./objs"
+OBJDIR="./objs"
 TOOL="./tool"
 
 do_clean() {
     echo "clean up"
-    rm -rf a.img bochsout.txt $OBJS/*.o;
+    rm -rf a.img bochsout.txt $OBJDIR/*.bin $OBJDIR/*.[oO];
 }
 
 do_compile() {
     echo "building boot"
-    $NASM $BOOT/boot.s  -o $OBJS/boot.o;
-    $NASM $BOOT/setup.s -o $OBJS/setup.o;
-    $NASM $BOOT/head.s  -o $OBJS/head.o;
+    $NASM $BOOT/boot.s  -o $OBJDIR/boot.O;
+    $NASM $BOOT/setup.s -o $OBJDIR/setup.O;
+    $NASM $BOOT/head.s  -o $OBJDIR/head.O;
 
     echo "building kernel"
-    $GCC  $KERNEL/main.c   -o $OBJS/main.o;
-    $GCC  $KERNEL/screen.c -o $OBJS/screen.o;
-    $GCC  $KERNEL/irq.c    -o $OBJS/irq.o;
-    $GCC  $KERNEL/timer.c  -o $OBJS/timer.o;
-    $GCC  $KERNEL/asm.c    -o $OBJS/asm.o;
-    $GCC  $KERNEL/gdt.c    -o $OBJS/gdt.o;
-    $GCC  $KERNEL/idt.c    -o $OBJS/idt.o;
-    $GCC  $KERNEL/kb.c     -o $OBJS/kb.o;
-    $GCC  $KERNEL/cpu.c    -o $OBJS/cpu.o;
-    $GCC  $KERNEL/string.c -o $OBJS/string.o;
-    $GCC  $KERNEL/page.c   -o $OBJS/page.o;
-    $GCC  $KERNEL/bitmap.c -o $OBJS/bitmap.o;
+    flist=`ls $KERNEL/`
+    for f in $flist
+    do
+
+	cmd="$GCC $KERNEL/$f -o $OBJDIR/${f/.c/.o}"
+	#echo $cmd
+	`$cmd`;
+    done
+#     $GCC  $KERNEL/main.c   -o $OBJS/main.o;
+#     $GCC  $KERNEL/screen.c -o $OBJS/screen.o;
+#     $GCC  $KERNEL/irq.c    -o $OBJS/irq.o;
+#     $GCC  $KERNEL/timer.c  -o $OBJS/timer.o;
+#     $GCC  $KERNEL/asm.c    -o $OBJS/asm.o;
+#     $GCC  $KERNEL/gdt.c    -o $OBJS/gdt.o;
+#     $GCC  $KERNEL/idt.c    -o $OBJS/idt.o;
+#     $GCC  $KERNEL/kb.c     -o $OBJS/kb.o;
+#     $GCC  $KERNEL/cpu.c    -o $OBJS/cpu.o;
+#     $GCC  $KERNEL/string.c -o $OBJS/string.o;
+#     $GCC  $KERNEL/page.c   -o $OBJS/page.o;
+#     $GCC  $KERNEL/bitmap.c -o $OBJS/bitmap.o;
 
     echo "linking"
-    cd  $OBJS;
-    ld boot.o -o boot.bin -T ../$TOOL/boot.ld;
-    ld setup.o -o setup.bin -T ../$TOOL/setup.ld;
+    cd  $OBJDIR;
+    ld boot.O -o boot.bin -T ../$TOOL/boot.ld;
+    ld setup.O -o setup.bin -T ../$TOOL/setup.ld;
+    
+    objs=`ls *.o`
+    cmd="ld head.O $objs -o kernel.bin -T ../$TOOL/kernel.ld"
+    echo $cmd
+    `$cmd`;
 
-    ld head.o main.o screen.o gdt.o idt.o asm.o irq.o \
-	timer.o kb.o cpu.o string.o page.o bitmap.o \
-	-o kernel.bin -T ../$TOOL/kernel.ld;
+#     ld head.o screen.o main.o gdt.o idt.o asm.o irq.o \
+# 	timer.o kb.o cpu.o string.o page.o bitmap.o \
+# 	-o kernel.bin -T ../$TOOL/kernel.ld;
     
     if [ $? -ne 0 ]
 	then
