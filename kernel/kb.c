c68a20510aa5faa2d501f701b5a530a9f31f5fd5
Yukang <moorekang@gmail.com>
2013-05-05 01:22:04 +0800

    continue kb

--- kernel/kb.c
+++ kernel/kb.c
@@ -14,20 +14,16 @@
 
 static int shift_state = 0 ;
 
-/* Handles the keyboard interrupt */ 
-void keyboard_handler(void) {
-    unsigned char scancode;
-    scancode = inb(0x60); //read the data buffer
-
-//    printk_hex((int)scancode);
-    if(scancode == 0x2a || scancode == 0x36)
+void kb_handler(void) {
+    u8 code = inb(0x60); //read the data buffer
+    if(code == 0x2a || code == 0x36)
         shift_state = 1;
-    else if(scancode == 0xaa || scancode == 0xb6 )
+    else if(code == 0xaa || code == 0xb6 )
         shift_state = 0;
     
     /* If the top bit of the byte we read from the keyboard is
     *  set, that means that a key has just been released */
-    if (scancode & 0x80) {
+    if (code & 0x80) {
         /* You can use this one to see if the user released the
         *  shift, alt, or control keys... */
         
@@ -37,13 +33,13 @@ void keyboard_handler(void) {
         *  hold a key down, you will get repeated key press
         *  interrupts. */
         putch(shift_state?
-              kbdus_upper[scancode]:kbdus[scancode]);
+              kbdus_upper[code]:kbdus[code]);
     }
 }
 
 void kb_init() {
     puts("kb init ...\n");
     irq_enable(1);
-    irq_install_handler(33, (isq_t)(&keyboard_handler));
+    irq_install_handler(33, (isq_t)(&kb_handler));
 }
 
